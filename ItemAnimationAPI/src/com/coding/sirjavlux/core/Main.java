88ae9ffe3df5a701eb32bdf6c32584282d943993
PluginBuild <sirjavlux12345@gmail.com>
2020-04-26 16:50:23 +0200

    Made some minor improvements and fixes

--- ItemAnimationAPI/src/com/coding/sirjavlux/core/Main.java
+++ ItemAnimationAPI/src/com/coding/sirjavlux/core/Main.java
@@ -1,20 +1,12 @@
 package com.coding.sirjavlux.core;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import com.coding.sirjavlux.entity.CustomEntityBat;
 import com.coding.sirjavlux.entity.RegisterEntity;
 
-public class Main extends JavaPlugin implements Listener {
+public class Main extends JavaPlugin {
 
 	private static NMSService nmsService;
 	private String consolePrefix = ChatColor.GRAY + "[ItemAnimationAPI]";
@@ -24,7 +16,6 @@ public class Main extends JavaPlugin implements Listener {
 		nmsService = new NMSService();
 		RegisterEntity.registerEntity("Bat", 65, CustomEntityBat.class);
 		
-		this.getServer().getPluginManager().registerEvents(this, this);
 		this.getServer().getConsoleSender().sendMessage(consolePrefix + ChatColor.GREEN + " Successfully enabled!");
 	}
 	
@@ -35,27 +26,5 @@ public class Main extends JavaPlugin implements Listener {
 	
 	public static NMSService getNMSService() {
 		return nmsService;
-	}
-	
-	@EventHandler 
-	public void pJoin(PlayerJoinEvent e) {
-		//plugin test animation
-		new BukkitRunnable() {
-			Location center = e.getPlayer().getLocation().clone();
-			double r = 1.3;
-			MovingItem item = new MovingItem(e.getPlayer(), new ItemStack(Material.ANVIL), center);
-			int degrees = 0;
-			@Override
-			public void run() {
-				degrees += 2;
-				double angle = degrees * Math.PI / 180;
-	            double x = center.getX() + r * Math.cos(angle);
-	            double z = center.getZ() + r * Math.sin(angle);
-	            double y = center.getY();
-	            item.teleport(new Location(Bukkit.getWorld("world"), x, y, z));
-				if (degrees >= 360) degrees = 0;
-			}
-			
-		}.runTaskTimer(this, 1, 1);
 	}
 }
--- ItemAnimationAPI/src/com/coding/sirjavlux/core/MovingItem.java
+++ ItemAnimationAPI/src/com/coding/sirjavlux/core/MovingItem.java
@@ -1,9 +1,5 @@
 package com.coding.sirjavlux.core;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
@@ -15,55 +11,32 @@ public class MovingItem {
 
     private Item item;
     private CustomEntityBat bat;
-    private List<Player> players;
+    private Player p;
     
     public MovingItem(Player p, ItemStack itemStack, Location location) {
         this.bat = Main.getNMSService().spawnBat(p, location);
         this.item = Main.getNMSService().spawnItem(p, itemStack.clone(), location, bat);
-        this.players = new ArrayList<>();
-        players.add(p);
-    }
-
-    public MovingItem(ItemStack itemStack, Location location) {
-        this.players = new ArrayList<>();
-        for (Player p : Bukkit.getOnlinePlayers()) {
-        	this.bat = Main.getNMSService().spawnBat(p, location);
-        	this.item = Main.getNMSService().spawnItem(p, itemStack.clone(), location, bat);
-            players.add(p);
-        }
+        this.p = p;
     }
     
-    public MovingItem(List<Player> players, ItemStack itemStack, Location location) {
-        this.players = new ArrayList<>();
-        for (Player p : players) {
-        	this.bat = Main.getNMSService().spawnBat(p, location);
-        	this.item = Main.getNMSService().spawnItem(p, itemStack.clone(), location, bat);
-            players.add(p);
-        }
-    }
-    
-    private MovingItem(List<Player> players, Item item, CustomEntityBat bat) {
-    	this.players = players;
+    private MovingItem(Player p, Item item, CustomEntityBat bat) {
+    	 this.p = p;
     	this.item = item;
     	this.bat = bat;
     }
 
 	public void despawn() {
-		for (Player p : players) {
-			if (p.isOnline()) {
-				Main.getNMSService().destroyEntity(p, item);
-				Main.getNMSService().destroyEntity(p, bat);	
-			}
+		if (p.isOnline()) {
+			Main.getNMSService().destroyEntity(p, item);
+			Main.getNMSService().destroyEntity(p, bat);	
 		}
     }
 
     public void teleport(Location location) {
-    	for (Player p : players) {
-    		if (p.isOnline()) Main.getNMSService().setLocation(p, bat, location);
-    	}
+    	if (p.isOnline()) Main.getNMSService().setLocation(p, bat, location);
     }
 
     public MovingItem clone() {
-        return new MovingItem(players, item, bat);
+        return new MovingItem(p, item, bat);
     }
 }
